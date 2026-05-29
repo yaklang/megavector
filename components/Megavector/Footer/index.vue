3959d672624f1984331a60383f297823bb4d13e6
song_xiao_lin <song_xiao_lin@126.com>
2026-05-29 10:57:02 +0800

    feat: 备案调整

--- components/Megavector/Footer/index.vue
+++ components/Megavector/Footer/index.vue
@@ -4,9 +4,12 @@
 <template>
   <div class="bg-[#000] h-[105px]">
     <div class="wjaq-container mx-auto h-[100%] flex justify-between items-center">
-      <div class=""><NuxtImg src="/Images/logo_transparent.png" class="w-[95px]" /></div>
+      <div class="">
+        <NuxtImg src="/Images/logo_transparent.png" class="w-[95px]" />
+      </div>
       <div class="footer-text text-[14px] text-[#A8A9AA]">
-        <span>Copyright 2013© 四维创智(北京)科技发展有限公司 All Rights Reserved 京ICP备17047700号-1</span>
+        <span>Copyright 2013© 四维创智(北京)科技发展有限公司 All Rights Reserved <a href="https://beian.miit.gov.cn/"
+            target="_blank">京ICP备17047700号-4</a></span>
         <span class="mx-[25px]">邮编：100085</span>
         <span>地址：北京市海淀区上地街道金隅嘉华大厦F座804</span>
       </div>
