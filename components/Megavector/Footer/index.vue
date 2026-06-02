fc93ef4dba7d8be3178d187ca8794a69ce1defab
song_xiao_lin <63901989+song-xiao-lin@users.noreply.github.com>
2026-06-02 10:34:30 +0800

    Song/feat/ga filing adjust (#32)
    * feat: 京公网安备11010802048713号
    
    * style: 字体大小调整

--- components/Megavector/Footer/index.vue
+++ components/Megavector/Footer/index.vue
@@ -7,9 +7,11 @@
       <div class="">
         <NuxtImg src="/Images/logo_transparent.png" class="w-[95px]" />
       </div>
-      <div class="footer-text text-[14px] text-[#A8A9AA]">
+      <div class="footer-text text-[12px] text-[#A8A9AA]">
         <span>Copyright 2013© 四维创智(北京)科技发展有限公司 All Rights Reserved <a href="https://beian.miit.gov.cn/"
-            target="_blank">京ICP备17047700号-4</a></span>
+            target="_blank">京ICP备17047700号-4</a>&nbsp;<a
+            href="https://beian.mps.gov.cn/#/query/webSearch?code=11010802048713" rel="noreferrer"
+            target="_blank">京公网安备11010802048713号</a></span>
         <span class="mx-[25px]">邮编：100085</span>
         <span>地址：北京市海淀区上地街道金隅嘉华大厦F座804</span>
       </div>
